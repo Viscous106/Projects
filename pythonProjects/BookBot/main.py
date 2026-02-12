314f99617ea3ded1c567a2114ab3b6b1da82aeff
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-02-12 16:11:31 +0530

    learned how to use sys library  to read

--- pythonProjects/BookBot/main.py
+++ pythonProjects/BookBot/main.py
@@ -1,10 +1,16 @@
+import sys
 from stats import count_words, count_characters, sort_on_values
+
 def get_book_text(book_path):
     with open(book_path, 'r', encoding='utf-8') as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1]
     text = get_book_text(path)
     word_count = count_words(text)
     char_count = count_characters(text)
@@ -15,5 +21,5 @@ def main():
     for item in sorted_chars:
         print(f"{item['char']}: {item['num']}")
     print("--- End report ---")
-    
+
 main()
